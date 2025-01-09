a51308e9d0fced536da9f4a0f9852696824f9b60
Nicole <76132106+nicolecomputer@users.noreply.github.com>
2025-01-09 13:03:11 -0700

    basic todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,84 @@
+import React from 'react'
 import './App.css'
+
+type TodoItem = {
+  title: string
+  id: number
+}
+
+type TodoItemProps = {
+  todo: TodoItem
+  onDelete: () => void
+}
+function TodoItem({ todo, onDelete }: TodoItemProps) {
+  return (
+    <li>
+      <span>{todo.title}</span>
+      <button onClick={onDelete}>Delete</button>
+    </li>
+  )
+}
+
+//
+type AddTodoItemProps = {
+  onAddItem: (title: string) => void
+}
+function AddTodoItem({ onAddItem }: AddTodoItemProps) {
+  const [itemValue, setItemValue] = React.useState<string>("")
+  return (
+    <div>
+      <input type="text" placeholder="Add your task"
+        value={itemValue}
+        onChange={(event) => {
+          setItemValue(event.target.value)
+        }} />
+      <div>
+        <button onClick={() => {
+          onAddItem(itemValue)
+        }}>Submit</button>
+      </div>
+    </div>
+  )
+}
+
+// Utility function
+function addTodoItem(todos: TodoItem[], title: string, currentId: number): TodoItem[] {
+  currentId++
+  return [
+    ...todos,
+    {
+      title: title,
+      id: currentId
+    }
+  ]
+}
+
+function removeTodoItem(todos: TodoItem[], id: number): TodoItem[] {
+  return todos.filter(todo => todo.id != id)
+}
+
+
 export default function App() {
+  const [items, setItems] = React.useState([
+    { title: 'water the plants', id: 1 },
+    { title: 'wash the dishes', id: 2 },
+    { title: 'walk the dog', id: 3 }
+  ])
+  const [currentId, setCurrentId] = React.useState(100)
   return (
     <div>
       <h1>Todo List</h1>
-      <div>
-        <input type="text" placeholder="Add your task" />
-        <div>
-          <button>Submit</button>
-        </div>
-      </div>
+      <AddTodoItem onAddItem={(title) => {
+        setItems(addTodoItem(items, title, currentId))
+        setCurrentId(currentId + 1)
+      }} />
       <ul>
-        <li>
-          <span>Walk the dog</span>
-          <button>Delete</button>
-        </li>
-        <li>
-          <span>Water the plants</span>
-          <button>Delete</button>
-        </li>
-        <li>
-          <span>Wash the dishes</span>
-          <button>Delete</button>
-        </li>
+        {items.map(item =>
+          <TodoItem todo={item} onDelete={() => {
+            setItems(removeTodoItem(items, item.id))
+          }} />
+        )}
+
       </ul>
     </div>
   );
